d9ae2a99733b76ac382237310b39db6c08bcb533
jamesascarter <jamesascarter@gmail.com>
2015-02-11 21:29:28 +0000

    removed all test from template and replaced with my own index page

--- app/server.rb
+++ app/server.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
 require 'sinatra/partial'
 
-set :partial_template_engine, :erb
+
 set :public_folder, Proc.new { File.join(root, '..', 'public') }
+set :views, Proc.new { File.join(root, 'views') }
+set :partial_template_engine, :erb
 
 get '/' do
   erb :index
